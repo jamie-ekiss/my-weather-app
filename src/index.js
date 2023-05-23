48c9010fd4b87d98b8cc18ee4983d45d0029f480
jamie-ekiss <132628536+jamie-ekiss@users.noreply.github.com>
2023-05-22 17:38:45 -0700

    Updated styling

--- src/index.js
+++ src/index.js
@@ -82,7 +82,7 @@ function displayTemperature(response) {
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
